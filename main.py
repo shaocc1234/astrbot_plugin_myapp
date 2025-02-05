9139d813791ecfc263d780761d3f9d4b6d72d3f9
shaocc1234 <363606263@qq.com>
2025-02-05 22:39:51 +0800

    hello

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from astrbot.api.all import *
+from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.star import Context, Star, register
 
-@register("astrbot_plugin_myapp", "Your Name", "一个简单的消息回复插件", "1.0.0")
+@register("helloworld", "cc", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
     
-    # 注册指令的装饰器。指令名为 hello。注册成功后，发送 `/hello` 就会触发这个指令
-    @filter.command("hello")
-    async def hello(self, event: AstrMessageEvent):
-        yield event.plain_result("你好，收到消息") # 发送一条纯文本消息
+    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
+    @filter.command("helloworld")
+    async def helloworld(self, event: AstrMessageEvent):
+        user_name = event.get_sender_name()
+        yield event.plain_result(f"Hello, {user_name}!") # 发送一条纯文本消息
